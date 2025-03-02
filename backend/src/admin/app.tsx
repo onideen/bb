dfbaf4e2c834973362d3a1c3dd335d9c746716ae
Vegar Engen <vegar.engen@gmail.com>
2025-03-02 13:55:45 +0100

    Vlr ffikse bildefiler vises

---
+++ backend/src/admin/app.tsx
@@ -0,0 +1,44 @@
+import type { StrapiApp } from "@strapi/strapi/admin";
+import Logo from "./extensions/my-auth-logo.png";
+
+export default {
+  config: {
+    auth: {
+      logo: Logo,
+    },
+    menu: {
+      logo: Logo,
+    },
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  bootstrap(app: StrapiApp) {
+    console.log(app);
+  },
+};
