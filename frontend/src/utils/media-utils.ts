680071f027757568baa1ff0761cbabc75a6d5068
Vegar Engen <vegar.engen@gmail.com>
2025-04-05 09:42:42 +0200

    Baseurl only if needed

--- frontend/src/utils/media-utils.ts
+++ frontend/src/utils/media-utils.ts
@@ -2,17 +2,22 @@ import { MediaAttributes } from "../components/MediaRenderer";
 
 export const getOptimalImage = (media?: MediaAttributes) => {
   if (!media) return "";
-
-  const baseUrl = import.meta.env.VITE_STRAPI_URL || ""; // Bytt til riktig Strapi-server URL
-
+  if (!media.formats) return addBaseUrlIfNeeded(media.url); // Return original image if no formats available
   const width = window.innerWidth;
 
   if (width <= 500 && media.formats?.small)
-    return baseUrl + media.formats.small.url;
-  if (width <= 750 && media.formats?.medium)
-    return baseUrl + media.formats.medium.url;
+    return addBaseUrlIfNeeded(media.formats.small.url);
+  else if (width <= 750 && media.formats?.medium)
+    return addBaseUrlIfNeeded(media.formats.medium.url);
   if (width > 750 && media.formats?.large)
-    return baseUrl + media.formats.large.url;
+    return addBaseUrlIfNeeded(media.formats.large.url);
 
-  return baseUrl + media.url; // Fallback til originalbildet
+  return addBaseUrlIfNeeded(media.url); // Fallback til originalbildet
 };
+
+function addBaseUrlIfNeeded(url: string): string {
+  if (url.startsWith("http")) return url;
+
+  const baseUrl = import.meta.env.VITE_STRAPI_URL || ""; // Bytt til riktig Strapi-server URL
+  return baseUrl + url;
+}
